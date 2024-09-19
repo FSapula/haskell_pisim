cce1bc08ae4a87d09b1582e9c49b9bb46bac2ddf
filip <filip125@icloud.com>
2024-09-19 12:49:30 +0200

    fixes

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,6 @@ import PhysicsSim
 someFunc :: IO ()
 someFunc = do
   let box1 = Box 1 1 0 0
-  let box2 = Box 5 1000000 (-1) 0
-  let sim = Sim box1 box2
-  print $ runState simulationStep (0, (Sim box1 box2))
+  let box2 = Box 5 10000000000 (-1) 0
+  let sim = Sim box1 box2 0
+  print $ runSimNoLog sim
--- src/PhysicsSim.hs
+++ src/PhysicsSim.hs
@@ -1,8 +1,12 @@
 module PhysicsSim (
-  simulationStep,
   Box (..),
   Simulation (..),
   module Control.Monad.State,
+  runSimLog,
+  boxMove,
+  Time,
+  SimulationLog,
+  runSimNoLog,
 ) where
 
 import Control.Applicative
@@ -18,38 +22,40 @@ data Box = Box
 data Simulation = Sim
   { box1 :: Box
   , box2 :: Box
+  , time :: Time
   }
   deriving (Show)
-data NextCollision = Boxes Double | Wall Double deriving (Eq, Ord, Show)
+data Collision = Boxes Double | Wall Double deriving (Eq, Ord, Show)
 type Time = Double
+type SimulationLog = [Simulation]
 
 timeToWall :: Simulation -> Maybe Double
-timeToWall (Sim box _)
+timeToWall (Sim box _ _)
   | velocity box >= 0 = Nothing
-  | otherwise = Just $ abs $ (position box - width box) / velocity box
+  | otherwise = return $ abs $ (position box - width box) / velocity box
 
 timeToBox :: Simulation -> Maybe Double
-timeToBox (Sim box1 box2)
+timeToBox (Sim box1 box2 _)
   | velocity box2 >= velocity box1 = Nothing
   | otherwise = return $ dist / relvelocity
  where
   dist = (position box2 - width box2) - (position box1 + width box1)
   relvelocity = velocity box1 - velocity box2
 
-getNextCollision :: Simulation -> Maybe NextCollision
+getNextCollision :: Simulation -> Maybe Collision
 getNextCollision sim = min <$> wallT <*> boxT <|> wallT <|> boxT
  where
   wallT = Wall <$> timeToWall sim
   boxT = Boxes <$> timeToBox sim
 
 boxMove :: Time -> Simulation -> Simulation
-boxMove t (Sim box1 box2) = Sim new1 new2
+boxMove t (Sim box1 box2 simt) = Sim new1 new2 (simt + t)
  where
   new1 = box1{position = position box1 + velocity box1 * t}
   new2 = box2{position = position box2 + velocity box2 * t}
 
 calcCollisionSpeed :: Simulation -> Simulation
-calcCollisionSpeed (Sim box1 box2) = Sim new1 new2
+calcCollisionSpeed (Sim box1 box2 t) = Sim new1 new2 t
  where
   v1 = velocity box1
   v2 = velocity box2
@@ -65,24 +71,37 @@ collideWithWall t = modify (\(i, s) -> (i + 1, newstate s))
  where
   newstate s = flipBoxVel (boxMove t s)
   flipBoxVel :: Simulation -> Simulation
-  flipBoxVel (Sim box1 box2) = Sim box1{velocity = (-1) * velocity box1} box2
+  flipBoxVel (Sim box1 box2 t) = Sim box1{velocity = (-1) * velocity box1} box2 t
 
 collideBoxes :: Time -> State (Int, Simulation) ()
 collideBoxes t = modify (\(i, s) -> (i + 1, newstate s))
  where
   newstate s = calcCollisionSpeed $ boxMove t s
 
-simulationStep :: State (Int, Simulation) (String)
-simulationStep = do
-  (i, s) <- get
-  case getNextCollision s of
-    (Just (Wall t)) -> do
-      collideWithWall t
-      simulationStep
-      return $ "Wall:" ++ show t
-    (Just (Boxes t)) -> do
-      collideBoxes t
-      simulationStep
-      return $ "Box:" ++ show t
-    Nothing -> do
-      return "End"
+simStep :: Collision -> State (Int, Simulation) ()
+simStep col = case col of
+  (Wall t) -> collideWithWall t
+  (Boxes t) -> collideBoxes t
+
+runSimLog :: Simulation -> SimulationLog
+runSimLog sim = runSimHelper sim []
+ where
+  runSimHelper :: Simulation -> SimulationLog -> SimulationLog
+  runSimHelper sim simlog = do
+    case getNextCollision sim of
+      Nothing -> simlog
+      (Just x) -> do
+        let (_, newsim) = execState (simStep x) (0, sim)
+        runSimHelper newsim (simlog ++ [newsim])
+
+runSimNoLog :: Simulation -> Int
+runSimNoLog sim = evalState (runSimHelper) (0, sim)
+ where
+  runSimHelper :: State (Int, Simulation) (Int)
+  runSimHelper = do
+    (i, s) <- get
+    case getNextCollision s of
+      Nothing -> return i
+      (Just x) -> do
+        simStep x
+        runSimHelper
